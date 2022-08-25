3117d2a70d3c89496092ff982a2c3f6d76687734
joaquinlmartin <joaquinlmartin@gmail.com>
2022-08-25 22:18:18 +0200

    iterations 1 and 2 done

--- hello-world/src/components/ToDoItem.vue
+++ hello-world/src/components/ToDoItem.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
+    <h1>{{ message }}</h1>
      <input type="checkbox" id="todo-item" />
     <label for="todo-item">My Todo Item</label>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return { message: 'This is the ToDoItem component' }
+  }
+}
 </script>
